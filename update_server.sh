a866b82727f2c5e1022dd086cf54ef2595032968
Clemens Puehringer <cl.puehringer@gmail.com>
2016-05-08 14:04:13 +0200

    hm...

--- update_server.sh
+++ update_server.sh
@@ -6,23 +6,25 @@ SERVER_DIR=factorio
 TARBALL_NAME=factorio.tar.gz
 HEADLESS_HOMEPAGE=https://www.factorio.com/download-headless/stable
 
+function as_user() {
+    su factorio $1
+}
+
 service factorio stop
 
-su factorio <<'EOF'
-cd $HOME
+cd /home/$USER
 rm -R $SERVER_DIR
 
 echo "downloading headless server..."
 $SETUP_DIRECTORY/parse_headless.py $HEADLESS_HOMEPAGE | wget -i - -O $TARBALL_NAME
 echo "extracting tarball..."
-tar -xzf $TARBALL_NAME
+sudo -u $USER tar xzf $TARBALL_NAME
 
 echo "linking dropbox..."
-ln -s /home/$USER/Dropbox/factorio /home/$USER/$SERVER_DIR/saves
+sudo -u $USER ln -s /home/$USER/Dropbox/factorio /home/$USER/$SERVER_DIR/saves
 
 NEWEST_SAVE=$( ls -t /home/$USER/Dropbox/factorio | head -n 1 )
 echo "starting server with save $NEWEST_SAVE..."
 timeout 5 "$SERVER_DIR/bin/x64/factorio --start-server $NEWEST_SAVE"
-EOF
 
 service factorio start
