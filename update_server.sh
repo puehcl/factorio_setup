8d2d9f894cff92cbd6d5a356822504f569b407a8
Clemens Puehringer <cl.puehringer@gmail.com>
2016-05-08 14:12:24 +0200

    ...

--- update_server.sh
+++ update_server.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 SETUP_DIRECTORY="$( cd "$( dirname "$0" )" && pwd )"
-USER=factorio
+FAC_USER=factorio
 SERVER_DIR=factorio
 TARBALL_NAME=factorio.tar.gz
 HEADLESS_HOMEPAGE=https://www.factorio.com/download-headless/stable
 
 service factorio stop
 
-cd /home/$USER
+cd /home/$FAC_USER
 rm -R $SERVER_DIR
 
 echo "downloading headless server..."
 $SETUP_DIRECTORY/parse_headless.py $HEADLESS_HOMEPAGE | wget -i - -O $TARBALL_NAME --no-check-certificate
 echo "extracting tarball..."
-sudo -u $USER tar xzf $TARBALL_NAME
+su $FAC_USER tar xzf $TARBALL_NAME
 
 echo "linking dropbox..."
-sudo -u $USER ln -s /home/$USER/Dropbox/factorio /home/$USER/$SERVER_DIR/saves
+su $FAC_USER ln -s /home/$FAC_USER/Dropbox/factorio /home/$FAC_USER/$SERVER_DIR/saves
 
-NEWEST_SAVE=$( ls -t /home/$USER/Dropbox/factorio | head -n 1 )
+NEWEST_SAVE=$( ls -t /home/$FAC_USER/Dropbox/factorio | head -n 1 )
 echo "starting server with save $NEWEST_SAVE..."
 timeout 5 "$SERVER_DIR/bin/x64/factorio --start-server $NEWEST_SAVE"
 
